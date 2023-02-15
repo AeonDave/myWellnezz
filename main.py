88826c82d71e52dc8717a16b3e23436c16f983b0
Dave <nova.davide@gmail.com>
2023-02-15 18:36:32 +0100

    new privacy sec

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import constants
 from main_loop import main
 from modules import update
+from modules.math_util import get_unique
 
 if __name__ == '__main__':
     update.check_new_versions(os.path.dirname(update.self_path()), constants.name, constants.version)
--- modules/math_util.py
+++ modules/math_util.py
@@ -1,5 +1,6 @@
 import base64
 import codecs
+import uuid
 from hashlib import sha256
 
 
@@ -11,8 +12,12 @@ def obfuscate(d: bytes, k: bytes) -> bytes:
     return bytes(c ^ k[i % len(k)] for i, c in enumerate(d))
 
 
+def get_unique() -> bytes:
+    return sha256(uuid.UUID(int=uuid.getnode()).bytes).digest()
+
+
 def get_key(d: str) -> bytes:
-    return sha256(codecs.encode(d, 'rot13').encode('utf-8')).digest()
+    return sha256(obfuscate(codecs.encode(d, 'rot13').encode('utf-8'), get_unique())).digest()
 
 
 def read_obfuscation(k: str, d: str) -> str:
